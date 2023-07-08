cd6f721c2269b436a5896501b635a4e9b378241c
apple <dsc@ctfft.ij>
2023-07-08 15:48:21 +0300

    Task1-7

--- HW2.task7.py
+++ HW2.task7.py
@@ -1,2 +1,14 @@
 x=input("Enter the nuber:")
 x=int(x)
+for i in range(x):
+    print("*",end="")
+print()
+
+x=input("Enter the nuber:")
+x=int(x)
+i = 0
+s = ""
+while x>i:
+    s += "*"
+    i += 1
+print(s)
